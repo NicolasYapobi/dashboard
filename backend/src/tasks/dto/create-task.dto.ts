321850fb41c61521322957adbdb255e37d09dcc3
NicolasYapobi <nicolas.yapobi@epitech.eu>
2024-12-10 21:49:45 +0100

    backend - feat: add get and post routes

--- backend/src/tasks/dto/create-task.dto.ts
+++ backend/src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTaskDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateTaskDto {
+    @ApiProperty()
+    title: string;
+
+    @ApiProperty({ required: false })
+    description: string;
+
+    @ApiProperty({default: "A faire"})
+    status: string;
+}
